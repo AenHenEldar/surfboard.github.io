d4294d020ef710bfc8b5f63dfe8454fa043a16fa
Eldar <eldar.hidirow@gmail.com>
2022-02-04 11:10:44 +0300

    ТЗ №3 (Исправил замечания)

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const hamburgerMenuItem = document.querySelectorAll('.hamburger-menu__item');
 
-    hamburger.addEventListener('click', function() {
-        this.classList.toggle('open');
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('open');
         hamburgerMenu.classList.toggle('open');
+    })
+    hamburgerMenu.addEventListener('click', (e) => {
+        if(e.target.classList.contains('hamburger-menu__item') || e.target.classList.contains('hamburger-menu__link')) {
+            hamburger.classList.toggle('open');
+            hamburgerMenu.classList.toggle('open');
+        }
     })
 });
